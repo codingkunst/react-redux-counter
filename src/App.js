8e7b4074edb950109a83af6e789a7bf7d968cab5
codingkunst <ryoo6931@gmail.com>
2024-07-06 15:57:44 +0900

    code update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { useDispatch, useSelector } from 'react-redux'
 
 function App() {
+  
+  const dispatch = useDispatch(); // dispatch 생성
+
+  const number = useSelector((state) => state.counter.number);
+  console.log(number);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='app-style'>
+      <div className='count-style'>{number}</div>
+      {/* 버튼 */}
+      <div className='btn-style'>
+        {/* [-1] 버튼 */}
+        <button 
+          className='btn-minus'
+          onClick={() => {
+            dispatch({type: 'MINUS_ONE'});
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          -1
+        </button>
+        {/* +1 버튼 */}
+        <button
+          className='btn-plus'
+          onClick={() => {
+            dispatch({type: 'PLUS_ONE'});
+          }}
+        >
+          +1
+        </button>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
