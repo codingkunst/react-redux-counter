9aa580496e92271cfe68202f06b8678daa7aca06
codingkunst <ryoo6931@gmail.com>
2024-07-08 14:38:48 +0900

    payload add

--- src/App.js
+++ src/App.js
@@ -1,39 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import { useDispatch, useSelector } from 'react-redux'
 // import Action Creator
-import { minusOne, plusOne } from "./redux/modules/counter";
+import { addNumber, minusNumber } from "./redux/modules/counter";
 
 function App() {
   
-  const dispatch = useDispatch(); // dispatch 생성
+  // dispatch 생성
+  const dispatch = useDispatch();
 
-  const number = useSelector((state) => state.counter.number);
-  console.log(number);
+  // const number = useSelector((state) => state.counter.number);
+  // console.log(number);
+
+  const [number, setNumber] = useState();
+  const globalNumber = useSelector((state) => state.counter.number);
+
+  const onChangeHandler = (event) => {
+    const value = event.target.value;
+    setNumber(+value);
+  };
+
+  const onClickAddNumber = () => {
+    dispatch(addNumber(number));
+  };
+
+  const onClickMinusNumber = () => {
+    dispatch(minusNumber(number));
+  };
 
   return (
     <div className='app-style'>
-      <div className='count-style'>{number}</div>
+      <div className='count-style'>{globalNumber}</div>
+      {/* 입력 */}
+      <input className='input-style' type='number' value={number} onChange={onChangeHandler} />
       {/* 버튼 */}
       <div className='btn-style'>
         {/* [-1] 버튼 */}
-        <button 
+        {/* <button 
           className='btn-minus'
           onClick={() => {
             dispatch({type: minusOne});
           }}
         >
           -1
-        </button>
+        </button> */}
         {/* +1 버튼 */}
-        <button
+        {/* <button
           className='btn-plus'
           onClick={() => {
             dispatch({type: plusOne});
           }}
         >
           +1
-        </button>
+        </button> */}
+
+        {/* 빼기 버튼 */}
+        <button className='btn-minus' onClick={onClickMinusNumber}>-</button>
+        {/* 더하기 버튼 */}
+        <button className='btn-plus' onClick={onClickAddNumber}>+</button>
       </div>
     </div>
   )
--- src/redux/modules/counter.js
+++ src/redux/modules/counter.js
@@ -6,38 +6,62 @@ const initialState = {
 };
 
 // Action value
-const PLUS_ONE = 'PLUS_ONE';
-const MINUS_ONE = 'MINUS_ONE';
+// const PLUS_ONE = 'PLUS_ONE';
+// const MINUS_ONE = 'MINUS_ONE';
+const ADD_NUMBER = 'ADD_NUMBER';
+const MINUS_NUMBER = 'MINUS_NUMBER';
 
 // Action Creator
-export const plusOne = () => {
-  return {
-    type: PLUS_ONE,
-  };
-};
+// export const plusOne = () => {
+//   return {
+//     type: PLUS_ONE,
+//   };
+// };
 
-export const minusOne = () => {
+// export const minusOne = () => {
+//   return {
+//     type: MINUS_ONE,
+//   }
+// };
+
+export const addNumber = (payload) => {
   return {
-    type: MINUS_ONE,
+    type: ADD_NUMBER,
+    payload: payload,
   }
 };
 
+export const minusNumber = (payload) => {
+  return {
+    type: MINUS_NUMBER,
+    payload,
+  }
+}
+
 // 리듀서
 const counter = (state = initialState, action) => {
-  console.log(action);
+  console.log('action : ', action);
   switch (action.type) {
-    case MINUS_ONE:
+    // case MINUS_ONE:
+    //   return {
+    //     number: state.number - 1,
+    //   }
+    // case PLUS_ONE:
+    //   return {
+    //     number: state.number + 1,
+    //   }
+    case ADD_NUMBER:
       return {
-        number: state.number - 1,
+        number: state.number + action.payload,
       }
-    case PLUS_ONE:
+    case MINUS_NUMBER:
       return {
-        number: state.number + 1,
+        number: state.number - action.payload,
       }
     default:
       return state;
   }
 };
 
-// 모듈파일에서는 리듀서를 export default 한다.
+// 모듈 파일에서는 리듀서를 export default 한다
 export default counter;
